b32eaa2c953bfc8e6712e449d322c6271f702e19
John Watterson <john@breakthrewbrew.com>
2020-01-28 15:14:35 -0700

    Initial commit; Turn and Card class objects

---
+++ test/Turn-test.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Turn = require('../src/Turn.js');
+const Card = require('../src/Card.js');
+describe('Turn', function () {
+
+  it('should be a function', function () {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+
+  it('should be an instance of Turn', function () {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it('should accept two arguments when instantiating Turn', function () {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('function', card);
+    expect(turn.playerGuess).to.equal('function');
+    expect(turn.card).to.equal(card);
+  });
+
+  it('should have a method that returns the player\'s guess', function () {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('function', card);
+    expect(turn.returnGuess()).to.equal('function');
+  });
+
+  it('should have a method that returns the current card in play', function () {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('function', card);
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it('should have a method that evaluates a player\'s guess and returns T/F', function () {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.evaluateGuess()).to.equal(true);
+  });
+
+  it('should have a method that provides a player with feedback based on their guess', function () {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('function', card);
+    expect(turn.evaluateGuess()).to.equal(false);
+    expect(turn.giveFeedback()).to.equal('No Dice! Try Again!');
+  });
+
+});
